f7435b48acfa339af5f8d285b3d73a954e9a1b3a
Moxh <moxhsharma878@gmail.com>
2025-06-11 14:56:19 +0530

    addign new js notes

--- _ frontend-development-/javascript notes/js.js
+++ _ frontend-development-/javascript notes/js.js
@@ -11,5 +11,17 @@
 //var person = new Object();
 //person.name = "sharad"
 //person.age ="25"
+ 
 
+/*var person = new Array(10);
+person[0] = "sharad";
+person[1] = 28 
+ 
+
+var person = new Array(12,14,25,65,87,85,45,78,98,65,45,23);*/
+
+function sum(a, b) {
+  return a + b;
+  console.log(a+b);
+}
 
