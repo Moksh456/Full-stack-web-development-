54358318510b098f4b19f8f488118685af51913e
Moxh <moxhsharma878@gmail.com>
2025-07-03 00:05:46 +0530

    adding new js code

--- _ frontend-development-/practice code for html css js/script.js
+++ _ frontend-development-/practice code for html css js/script.js
@@ -1,10 +1,19 @@
 const frutis = [ "banana", "apple", "orange", "kiwi", "mango" ];
-a1[3] = "Mango";
-console.log(a1[3]);
-
+//a1[3] = "Mango";
+//console.log(a1[3]);
 const a1 = [7, " banana", true, "M", "orang"];
+//a1[3] = "Mango";
 
-  
 
-a1[3] = "Mango";
- 
+
+//console.log(a1.length); // Get the length of the array
+/*console.log(a1.pop());
+console.log(a1.length);*/
+
+//console.log(a1.push("Baljeet")); // Add an element to the end of the array
+//console.log(a1[6]); 
+
+//console.log(a1.shift()); // Remove the first element of the array
+//console.log(a1.length);
+//console.log(a1.shift("shantra"));
+
