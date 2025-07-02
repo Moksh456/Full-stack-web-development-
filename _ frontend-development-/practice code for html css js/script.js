296639a2473a2455463783cf77a31c217e3c4c56
Moxh <moxhsharma878@gmail.com>
2025-07-02 23:36:55 +0530

    adding new js practice code

--- _ frontend-development-/practice code for html css js/script.js
+++ _ frontend-development-/practice code for html css js/script.js
@@ -1,7 +1,7 @@
 const frutis = [ "banana", "apple", "orange", "kiwi", "mango" ];
 
-const a1 = [7, " banana", true, null, "orang"];
+const a1 = [7, " banana", true, "M", "orang"];
 
-console.log(a1[3]); // null
+console.log(a1.toString());
 
-
+console.log(typeof a1[3]);
