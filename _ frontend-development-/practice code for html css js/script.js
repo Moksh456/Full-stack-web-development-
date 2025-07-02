f513483a690e33965b43d29d2d108ecdeadd1fc0
Moxh <moxhsharma878@gmail.com>
2025-07-03 00:42:45 +0530

    adding new js code

--- _ frontend-development-/practice code for html css js/script.js
+++ _ frontend-development-/practice code for html css js/script.js
@@ -4,7 +4,7 @@ const frutis = [ "banana", "apple", "orange", "kiwi", "mango" ];
 const a1 = [7, " banana", true, "M", "orang"];
 //a1[3] = "Mango";
 
-let num = [1,2,3,4,5,6,7,8,9];
+/*let num = [1,2,3,4,5,6,7,8,9];
 
 //console.log(a1.length); // Get the length of the array
 /*console.log(a1.pop());
@@ -16,4 +16,44 @@ console.log(a1.length);*/
 //console.log(a1.shift()); // Remove the first element of the array
 //console.log(a1.length);
 //console.log(a1.shift("shantra"));
+a1.toString
+/*ƒ toString() { [native code] }
+typeof a1[0]
+'number'
+a1.toString
+ƒ toString() { [native code] }
+typeof a1[0]
+'number'
+a1.join
+ƒ join() { [native code] }
+a1.join(" and ")
+'7 and  banana and true and M and orang'
+a1.po
+undefined
+a1.pop();
+'orang'
+a1.push
+ƒ push() { [native code] }
+a1.push(5);
+5
+a1.shift();
+7
+delete a1[2]
+true
+num.concat(frutis);
+(14) [1, 2, 3, 4, 5, 6, 7, 8, 9, 'banana', 'apple', 'orange', 'kiwi', 'mango']
+let b = num.concat(frutis);
+undefined
+b
+(14) [1, 2, 3, 4, 5, 6, 7, 8, 9, 'banana', 'apple', 'orange', 'kiwi', 'mango']
+num.splice(3,6, 255,266,288)
+(6) [4, 5, 6, 7, 8, 9]
+num
+(6) [1, 2, 3, 255, 266, 288]
+num.slice(num.slice(2, 3)
+
+num.slice(2, 3)
+[3]
+num
+(6) [1, 2, 3, 255, 266, 288]*/
 
