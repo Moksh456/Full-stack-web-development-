e55adb249790aa5c401990a2a74bd69d41dbb149
Moksh <moxhsharma878@gmail.com>
2025-07-31 21:43:32 +0530

    add new file for the lexical enviorment

---
+++ _ frontend-development-/javascript notes/lescial enviorment/lexical.js
@@ -0,0 +1,24 @@
+var age = 28;
+var title = "sharad"
+function printdetails(){
+    console.log(age)
+
+    print();
+    function print(){
+     
+        console.log(`hello my name is ${title} and my age is ${age}`)
+    }
+}
+printdetails();
+//quetion :
+/* here title and age both the variables are not defined under print function 
+ then also we are able to access it How ? 
+ it is because of scope chain 
+ it appear toprototype chai stuided earlier 
+ though prototype chain was applicable to objects and it's propeties similarly
+  scope chain is applicable is variables 
+ */
+
+  /* lexical Env variables accessed local + it lexical parent
+    
+  */
