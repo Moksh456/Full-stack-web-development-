62dd1833fa6462f1a80f46bf5e85b9194ad61000
Moxh Sharma <moxhsharma878@gmail.com>
2025-08-06 23:40:18 +0530

    .

---
+++ _ frontend-development-/javascript notes/functional_class.html/funcitonal..js/functional.js
@@ -0,0 +1,10 @@
+/*PArent class called PERSON which needs to be initilized with certain propeties like name and age.
+
+    there is child class called employee (which is a PERSON to!) and it needs to be
+    initilized with properties like name, age, and employmenttype.
+*/
+
+function person(name, age){
+    this.name = name;
+    this.age = age;
+}
