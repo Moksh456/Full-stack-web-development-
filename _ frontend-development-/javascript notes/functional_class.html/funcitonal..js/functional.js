6eb7d660c7c2481114ace34008b3008baff4cdc0
Moxh Sharma <moxhsharma878@gmail.com>
2025-08-07 22:02:41 +0530

    .

--- _ frontend-development-/javascript notes/functional_class.html/funcitonal..js/functional.js
+++ _ frontend-development-/javascript notes/functional_class.html/funcitonal..js/functional.js
@@ -11,8 +11,14 @@ function person(name, age){
  
 // here person.Prototype.consturctor again points hack to person 
 // does that mean -> person.prototype.constructor  === person will be true?
+// person.prototype =.constructor is set automatiocally to person.
 
 
 const p1 = new person("sharad", 28);
 
 //new object is created and prototype of this newly created object is set to person.prototype
+// this keyword is pointed to newly created object.
+
+// to access prototype of p1 -> object.getPrototypeOf(p1) can used 
+
+
