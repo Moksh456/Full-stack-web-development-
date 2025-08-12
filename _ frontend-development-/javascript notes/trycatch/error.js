e29f5065c7118b8e49f2be24f5de02356a09ce4c
Moxh Sharma <moxhsharma878@gmail.com>
2025-08-12 22:34:31 +0530

    .

--- _ frontend-development-/javascript notes/trycatch/error.js
+++ _ frontend-development-/javascript notes/trycatch/error.js
@@ -1,6 +1,12 @@
 const sum = (a,b) => {
-    console.log(uiggugi)
-    return a+b+;
+    console.log("begin")
+    try {
+        console.log(person.tilte)
+    } catch (error) {
+        console.log("pure system...")
+    }
+    console.log("END OF THE CODE.....")
+    return a+b;
 }
 
 sum(25,30);
