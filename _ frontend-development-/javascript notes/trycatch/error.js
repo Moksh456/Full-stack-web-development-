25ccb59f409de873556472009aa5b807912a8744
Moxh Sharma <moxhsharma878@gmail.com>
2025-08-13 12:43:22 +0530

    .

--- _ frontend-development-/javascript notes/trycatch/error.js
+++ _ frontend-development-/javascript notes/trycatch/error.js
@@ -12,7 +12,7 @@
 sum(25,30);
 console.log("sum is ", sum(25,30));*/
 
-const sum = (a, b) => {
+/*const sum = (a, b) => {
   
   try{
       let result = a+b;
@@ -21,10 +21,40 @@ const sum = (a, b) => {
            
     }
     return result
-}
+}return result
 catch(error){
-        console.log(error)
+        console.log(error);
     }
 }
  const s = sum(25, 30);
+*/
+
+const sum = (a, b) => {
   
+  try{
+      let result = a+b;
+      if(result>10){
+            throw new SyntaxError("result  is more than 10...it wrong")
+           
+    }
+    // add more login -> if result is greater than 50 then Syntaxerror 
+
+    
+}
+catch(error){
+       if(error instanceof ReferenceError){
+            console.log(error.name);
+        console.log(error.message);
+        }
+        else{
+          return error
+        }
+    }
+}
+try{
+    const c = sum(1,2);
+}catch{
+    console.log("Global catch hanler");
+
+}
+
