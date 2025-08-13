e0c693e0a040e5e7cc08301fde6729d4b8ce9add
Moxh Sharma <moxhsharma878@gmail.com>
2025-08-13 12:12:39 +0530

    .

--- _ frontend-development-/javascript notes/trycatch/error.js
+++ _ frontend-development-/javascript notes/trycatch/error.js
@@ -1,4 +1,4 @@
-const sum = (a,b) => {
+/*const sum = (a,b) => {
     console.log("begin")
     try {
         console.log(person.tilte)
@@ -10,4 +10,21 @@ const sum = (a,b) => {
 }
 
 sum(25,30);
-console.log("sum is ", sum(25,30));
+console.log("sum is ", sum(25,30));*/
+
+const sum = (a, b) => {
+  
+  try{
+      let result = a+b;
+      if(result>10){
+            throw new Error("result  is more than 10...it wrong")
+           
+    }
+    return result
+}
+catch(error){
+        console.log(error)
+    }
+}
+ const s = sum(25, 30);
+  
