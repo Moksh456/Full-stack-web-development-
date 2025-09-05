b0ac6405c7b350f08b38ea42aa04613a5ebbb1c2
Moxh Sharma <moxhsharma878@gmail.com>
2025-09-05 14:01:16 +0530

    .

---
+++ _ frontend-development-/javascript notes/timer/timerPlayground.html/timerPlayground.js
@@ -0,0 +1,19 @@
+ // snipet 1
+// const id = setTimeout(()=>{
+// console.log("hello everyone...")
+// }, 1000)
+
+// clearTimeout(id)
+
+
+// snipet 2
+
+const id = setTimeout(() => {
+    console.log("hello everyone...")
+}, 1000);
+
+
+
+console.log("hello world");
+
+
