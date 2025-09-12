3d19054586b77dbc272f2097409182729c2ffb64
Moksh <moxhsharma878@gmail.com>
2025-09-12 22:20:54 +0530

    .

--- _ frontend-development-/javascript notes/promisies/promise.js
+++ _ frontend-development-/javascript notes/promisies/promise.js
@@ -1,3 +1,4 @@
+/*
 const res = new Promise((resolve, reject) => {;
 // nimic async flow via settimeout API
 
@@ -8,6 +9,39 @@ const res = new Promise((resolve, reject) => {;
 
 console.log(res);
 
+*/
+
+// How to create Promise by ourself?
+    
+const callback = (resolve, reject) =>{
+    reject("Promise resolved");
+    // here some heavylifting task might be going on 
+    // like getting data form somewhere 
+    // like heavy I/D
+    // like doing a for loop over millions of elements... that will take sometime
+    // in order to mimic that time...lets implement settimeout 
+
+    setTimeout(() => {
+        resolve("Promise resolved");
+    }, 4000);
+}
+
+// above callback will be called imediately.
+// Note: provided execution stack is empty.
+// Initially promise state is  PENDING  and promise data undefined
+
+// here resolve and reject are functions that should be called inside this 
+// callback function whenever we want to change state of promise form pending to 
+// fullfilled or pending to reject
+
+// While changing state of promise to FULLFILLED i.e calling resolve function 
+// whenever data we supply to resolve function will be set inside promise data.
+
+let res = new Promise(callback);
 
 
-fetch();
+console.log(res);
+
+// How do we know when promise state of promise changes ?
+// problem Statement: After Promise is fullfilled/resolved... I want to 
+// sonsole.log("Its done")
