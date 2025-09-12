e27efe254e8415fd5f98fe8b36f9c8ab707f23ee
Moksh <moxhsharma878@gmail.com>
2025-09-12 21:51:51 +0530

    .

--- _ frontend-development-/javascript notes/promisies/promise.js
+++ _ frontend-development-/javascript notes/promisies/promise.js
@@ -1,2 +1,7 @@
-const res = new Promise(() => {});
+const res = new Promise((resolve, reject) => {;
+// nimic async flow via settimeout API
+
+    setTimeout(() => {
+       resolve("Promise resolved");
+    }, 4000);})
 console.log(res);
