dd48830b65c5c84a10dd9ecf57be049d136b1b03
Moksh <moxhsharma878@gmail.com>
2025-09-25 22:48:58 +0530

    ,

--- _ frontend-development-/javascript notes/BOM/bom.js
+++ _ frontend-development-/javascript notes/BOM/bom.js
@@ -3,7 +3,29 @@
 // console.log(result);
 // console.log("pricntig after alert");
 
-// console.log("Printig before alert");
+// console.log("Printig before alert.......");
 // const result = prompt("Are you happy with your effots......")
 // console.log(result);
-// console.log("pricntig after alert");
+// console.log("pricntig after aler........t");
+
+
+//console.log("Printig before alert.......");
+// const result = confirm("Are you practising daily ?")
+// console.log(result);
+// console.log("pricntig after aler........t");
+
+
+
+console.log(location.href);
+console.log(location.protocol);
+console.log(location.port);
+console.log(location.origin);
+console.log(location.pathname)
+
+const result = confirm("do your want to navigate to dummy html page ?")
+
+if(result){
+    // navigate to dummy.html
+    location.href = "http://127.0.0.1:5500/dummy.html"
+
+}
