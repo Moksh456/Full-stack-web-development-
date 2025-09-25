102fa8dfceb7435b38cda68009451b06fc78457b
Moksh <moxhsharma878@gmail.com>
2025-09-25 22:20:16 +0530

    .

--- _ frontend-development-/javascript notes/BOM/bom.js
+++ _ frontend-development-/javascript notes/BOM/bom.js
@@ -1,3 +1,4 @@
-console.log("Printig before alert");
-alert("kindly be sincere and complele your assigments")
-console.log("pricntig after alert");
+// console.log("Printig before alert");
+// let result =alert("kindly be sincere and complele your assigments")
+// console.log(result);
+// console.log("pricntig after alert");
