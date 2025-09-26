a4e64094204440d89f79fe3ee39037b7b9725efa
Moksh <moxhsharma878@gmail.com>
2025-09-26 21:53:05 +0530

    .

--- _ frontend-development-/javascript notes/BOM/bom.js
+++ _ frontend-development-/javascript notes/BOM/bom.js
@@ -16,16 +16,24 @@
 
 
 
-console.log(location.href);
-console.log(location.protocol);
-console.log(location.port);
-console.log(location.origin);
-console.log(location.pathname)
+// console.log(location.href);
+// console.log(location.protocol);
+// console.log(location.port);
+// console.log(location.origin);
+// console.log(location.pathname);
 
-const result = confirm("do your want to navigate to dummy html page ?")
+console.log(location.search);
+console.log(location.hash);
+
+/*const result = confirm("do your want to navigate to dummy html page ?")
 
 if(result){
     // navigate to dummy.html
-    location.href = "http://127.0.0.1:5500/dummy.html"
+  //  location.href = "http://127.0.0.1:5500/dummy.html" 1st way
+// When ever we assign any value to location.href..internally location.assign()
+// method is called..
+  //  location.assign("http://127.0.0.1:5500/dummy.html") //2nd way
 
+    location.replace("http://127.0.0.1:5500/dummy.html") //3rd way
 }
+*/
