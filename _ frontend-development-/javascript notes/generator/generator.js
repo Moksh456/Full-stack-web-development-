8e45e4b3b0844a8f81bb1b209c445b974edee1eb
Moksh <moxhsharma878@gmail.com>
2025-09-27 23:50:19 +0530

    .

--- _ frontend-development-/javascript notes/generator/generator.js
+++ _ frontend-development-/javascript notes/generator/generator.js
@@ -1,11 +1,19 @@
-function func(){
+/*function func(){
     console.log("hi")
     console.log("by")
 
 }
 
-func();
+func();*/
 
 // problem statement 
 // anyone who is calling func should jave power via which we can stop  
 // resume execution of func at my given point.
+
+function* func(){
+       console.log("hi")
+    console.log("by")
+}
+
+const result = func();
+result.next();
