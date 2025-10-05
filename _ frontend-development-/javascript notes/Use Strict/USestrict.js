b2a41f502f75c7e3eccdabeda68bfa235f02398c
Moksh <moxhsharma878@gmail.com>
2025-10-05 22:02:19 +0530

    .

--- _ frontend-development-/javascript notes/Use Strict/USestrict.js
+++ _ frontend-development-/javascript notes/Use Strict/USestrict.js
@@ -1,8 +1,15 @@
-"use strict";
+  "use strict";
 // let dummy = "hi";
 // console.log(dummy);
 
-function demo(){
+/* function demo(){
     console.log(this);
 }
-demo();
+demo();*/
+
+/* function domo(p1, p1) {
+    console.log(p1);
+} */
+
+  //  let arguments = 20;
+  //  console.log(arguments);
