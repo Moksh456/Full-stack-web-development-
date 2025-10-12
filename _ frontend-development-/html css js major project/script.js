80cd19b05cbfa45971c312ba42d170f42bc7d115
Moksh <moxhsharma878@gmail.com>
2025-10-12 14:46:54 +0530

    .

---
+++ _ frontend-development-/html css js major project/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+const input0 = document.querySelector("#zero");
+const input1 = document.querySelector("#one");
+const input2 = document.querySelector("#two");
+const input3 = document.querySelector("#three");
+const input4 = document.querySelector("#four");
+const input5 = document.querySelector("#five");
+
+console.log(input0);
