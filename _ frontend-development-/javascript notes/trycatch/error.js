0aa8cff2a3afe8f807020b48099e7bebd626cac7
Moksh <moxhsharma878@gmail.com>
2025-10-13 22:53:58 +0530

    .

--- _ frontend-development-/javascript notes/trycatch/error.js
+++ _ frontend-development-/javascript notes/trycatch/error.js
@@ -30,29 +30,29 @@ catch(error){
 */
 
 const sum = (a, b) => {
-  
-  try{
-      let result = a+b;
-      if(result>10){
-            throw new SyntaxError("result  is more than 10...it wrong")
-           
-    } 
-    // add more login -> if result is greater than 50 then Syntaxerror 
 
-     
-}
-catch(error){
-       if(error instanceof ReferenceError){
-            console.log(error.name);
-        console.log(error.message);
+    try {
+        let result = a + b;
+        if (result > 10) {
+            throw new SyntaxError("result  is more than 10...it wrong")
+
         }
-        else{
-          return error
+        // add more login -> if result is greater than 50 then Syntaxerror 
+
+
+    }
+    catch (error) {
+        if (error instanceof ReferenceError) {
+            console.log(error.name);
+            console.log(error.message);
+        }
+        else {
+            return error
         }
     }
 }
-try{
-    const c = sum(1,2);
-}catch{
+try {
+    const c = sum(1, 2);
+} catch {
     console.log("Global catch hanler");
 }
