539277ca687537c33f73ff7e51d0d25ddd9fc29d
Moksh <moxhsharma878@gmail.com>
2026-05-30 22:26:38 +0530

    .

--- _ frontend-development-/javascript notes/functional_class.html/funcitonal..js/functional.js
+++ _ frontend-development-/javascript notes/functional_class.html/funcitonal..js/functional.js
@@ -23,7 +23,7 @@ function employee(name, age, employmentType) {
 employee.prototype = Object.create(person.prototype)
 // here employee.protye.constructor is lost because we are assign 
 // employee.prototype to something ELSE EXPLICTY.
-
+ 
 // Now we can reset constructor employee propety Exlicity
 employee.prototype.constructor = employee;
 
