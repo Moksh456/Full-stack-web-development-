8d4dc496f737c74bc6014580bfbe66f38e8178b4
Moksh <moxhsharma878@gmail.com>
2026-06-07 23:46:31 +0530

    .

--- javascript master/script.js
+++ javascript master/script.js
@@ -1,1 +1,54 @@
-console.log("hello world");
+// var a = (20*524*8) + 50.5;
+
+// console.log(a);
+
+// var let const  explaning each data container
+
+// var a;  // declare krna 
+// a = "hello miss" // first value initilised krna
+
+// var is function scoped
+//  window me add hota h 
+// dobara value change kr skte h 
+
+// a = 25;
+
+// console.log(a);
+
+
+//  var is very flexible and function scoped   but the let has some privcy and secutiry better than var and var safets mode is we use const
+
+//    const
+
+ //   let dulha = "lab";
+ //   const dulhan = "laby";
+
+ //   dulha = "haraMZADA"
+
+ //    console.log(dulha + "  webs " +dulhan);
+
+ // CONST VALUE IS PERMANENT 
+
+ // GLOBAL SCOPED , FUNCITON SCOPED , BLOCK SCOPED
+
+
+
+ //  REDECLARETION IS POSSIBLE WITH VAR BUT NOT POSSIBLE WITH LET
+
+ // temperol dead zone   .js ko pta oth h ki variable exist krta h lekin vo uski value use nhi kr skta
+
+
+//  console.log(a);
+
+// let a = 11;
+ 
+// hoisting impect per type
+// hoisting =>   ek variable ko jab js me banate h to vo variable do hisso me tututa h and uska devclare part upar chla jata h and usko insilisation part niche rh jata h.
+// var a = undefined;
+
+// a = 20;
+
+
+ // var -> hoist => undefined
+ // let -> hosit => X
+ // const -> hoist => X
