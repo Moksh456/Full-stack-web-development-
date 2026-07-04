45c1aafc9a564cb19cf11cd50cabb9a61808e4d6
moksh <moxhsharma878@gmail.com>
2026-07-04 23:50:59 +0530

    >

--- GOD LEVEL/group.js
+++ GOD LEVEL/group.js
@@ -1,7 +1,17 @@
-let a = document.querySelector("input");
+/* let a = document.querySelector("input");
 
 a.addEventListener("input", function(eve){
     if (eve.data !== null) {
         console.log(eve.data)
     }
+})*/
+
+let h1 = document.querySelector("h1");
+
+let doc = document.querySelector("select");
+
+doc.addEventListener("change", function(eve){
+    console.log(eve.target.value);
+
+    h1.innerText = `You have selected ${eve.target.value} device`;
 })
