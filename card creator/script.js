85cf20657fa99a76aa9a458aa794be2447cdf92c
moksh <moxhsharma878@gmail.com>
2026-07-20 23:29:55 +0530

    .

---
+++ card creator/script.js
@@ -0,0 +1,13 @@
+let card = document.querySelector("card");
+let h2 = document.createElement("h2").className = "h2"
+let h5 = document.createElement("h5").className = "h5"
+let p = document.createElement("p").className = "p"
+
+let input = document.querySelector("form");
+
+input.addEventListener("submit", function(Eve){
+    Eve.preventDefault();
+    
+      
+  
+})
