7b38f93d16565fd011dce8dbb4d71c5825596818
moksh <moxhsharma878@gmail.com>
2026-07-28 23:11:42 +0530

    .

--- form validiations/script.js
+++ form validiations/script.js
@@ -10,3 +10,9 @@ const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const emailans = emailRegex.test("")
 
 console.log(emailans);
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+});
