d0b487a3c911257bcf5514a41d3a6758bd146802
moksh <moxhsharma878@gmail.com>
2026-07-30 10:58:52 +0530

    .

--- timer codes/script.js
+++ timer codes/script.js
@@ -1,11 +1,20 @@
 let h1 = document.querySelector("h1")
 let i = 0
 
+
+
 document.querySelector("button").addEventListener("click", () => {
     clearInterval(timerid);
     i = 0;
     h1.textContent = i;
 });
+
+function hacked(a) {
+    let ans = a * 5;
+  console.log(ans);
+  
+}
+
 
 document.querySelector("button:nth-of-type(2)").addEventListener("click", () => {
     timerid = setInterval(() => {
@@ -13,3 +22,7 @@ document.querySelector("button:nth-of-type(2)").addEventListener("click", () => 
         h1.textContent = i;
     }, 1000)
 });
+
+hacked(5);
+
+console.log("heloo mrs");
