e46a638231e5c32bc1b9e61def08063e25cdbecf
moksh <moxhsharma878@gmail.com>
2026-08-04 22:25:53 +0530

    .

--- downloading bar/script.js
+++ downloading bar/script.js
@@ -1,12 +1,15 @@
 
 let width = 0;
-let bar = document.querySelector("process");
+let bar = document.querySelector(".process");
 
 let time = setInterval( function(){
-    if (width < 99) {
+    if (width <= 99) {
          width++;
          bar.style.width = `${width}%`
+         document.querySelector(".precentage").textContent = `${width}%`
+    }else{
+        clearInterval(time)
     }
-}, 30);
-
+    console.log(width)
+}, 100)
 
