bd7aaf7899514d2364fb3068a42a27d4c7c7532b
moksh <moxhsharma878@gmail.com>
2026-08-06 22:49:33 +0530

    .

--- theme toggler/script.js
+++ theme toggler/script.js
@@ -1,18 +1,22 @@
 // task is simple when the we want to change the theme of the page then we will also need to recover the previous theme 
 
 
-
 let btn = document.querySelector("button");
-let main = document.querySelector(".main");
+let main = document.querySelector("#container");
+
 
 function applyClass(){
-    console.log( main.classList.value);
-    
+
+    if(main.classList.value === "light"){
+        localStorage.setItem("theme", "dark")
+    }else{
+        localStorage.setItem("theme", "light")
+    }
+   
 }
 
 btn.addEventListener("click", function(){
-
-        
+    
 })
 
-applyClass();  
+applyClass();
